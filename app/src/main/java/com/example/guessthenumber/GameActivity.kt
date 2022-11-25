a5d61bb2cb6695048cf234a6f85b165613f5b4b3
Ivan Petr <ivan.kiel@gmail.com>
2022-11-25 09:11:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/guessthenumber/GameActivity.kt
@@ -0,0 +1,16 @@
+package com.example.guessthenumber
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+
+    fun onYesNoClick(view: View) {}
+
+
+}
